d2daa5a85a06be11f8faa8fbd0426b1a053d1cae
Kim Jae Hui <reysin960205@gmail.com>
2022-03-26 14:17:10 +0900

    satur

--- c_testing/test.c
+++ c_testing/test.c
@@ -1,4 +1,4 @@
-#include <stdio.h?
+#include <stdio.h>
 
 int main()
 {
@@ -6,5 +6,20 @@ int main()
     printf("%f \n", 3.14);
     printf("%g \n", 3.14);
     printf("%c \n", 'a');
+
+    int a = 1;
+    int b = 2;
+    int sum = a+b;
+    
+    printf("%d + %d = %d /n", a, b, a+b);
+    printf("%d + %d = %d /n", a, b, sum);
+
+
+
+
     return 0;
 }
+
+// git add .
+// git commit -m "history"
+// git push origin master
