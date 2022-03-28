3de24f6b933bd771912c9ee9f8534563056cdfe3
Kim Jae Hui <reysin960205@gmail.com>
2022-03-29 08:54:50 +0900

    100 problem

---
+++ c_testing/100제.c
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+	
+
+	return 0;
+}
+
+/* 1~8 printf로 우측 주어진 문장 출력*/
+	// printf("Hello");  - Hello
+	// printf("Hello World");  - Hello World
+	// printf("Hello\nWorld");  - Hello 다음줄 World
+	// printf("\'Hello\'");  - 'Hello'
+	// printf("\"Hello World\"");  - "Hello World"
+	// printf("\"!@#$$%^&*()\"");  - "!@#$%^&*()"
+	// printf("C:\\Download\\hello.cpp\"");  - "C:\Download\hello.cpp"
+	// printf("\u250C\u252C\u2510\n\u251C\u253C\u2524\n\u2514\u2534\u2518");  - 유니코드 출력
+
+/* 10~27 입&출력 */
+
+// git add .
+// git commit -m "history"
+// git push origin master
