870dd8c742b10020031401c77f65fa332df09903
Kim Jae Hui <reysin960205@gmail.com>
2022-03-28 09:54:14 +0900

    mond

--- c_testing/test.c
+++ c_testing/test.c
@@ -1,16 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <math.h>
+
 
 int main()
 {
-    int a=0 , b=0;
-    printf("숫자 1 입력 : \n");
-    scanf("%d", &a);
-    printf("숫자 2 입력 : \n");
-    scanf("%d", &b);
+	int a, b;
+	printf("숫자 1 입력 :"); scanf("%d", &a);
+	printf("숫자 2 입력 : ");scanf("%d", &b);
 
-    printf("두 숫자의 합은 %d입니다", a+b);
+	printf("두 숫자의 합은 %d입니다", a+b);
 
-    return 0;
+
+	float a_kg, b_m;
+	printf("체중을 입력하세요 :"); scanf("%f", &a_kg);
+	printf("키를 입력하세요 : "); scanf("%f", &b_m);
+
+	b_m = pow(b_m, 2);
+	printf("당신의 BMI는 %f입니다", a_kg / b_m);
+
+	return 0;
 }
 
 // git add .
