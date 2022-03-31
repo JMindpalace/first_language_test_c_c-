8ddd628386a2fc3ab65454dc022d82ef82b97ebf
Kim Jae Hui <reysin960205@gmail.com>
2022-03-31 16:14:01 +0900

    thursday testing end

--- c_testing/100제 풀이 중.c
+++ c_testing/100제 풀이 중.c
@@ -3,16 +3,14 @@
 
 int main()
 {
-	float a, b, c, d;
-	scanf("%f %f %f", &a, &b, &c, &d);
-	float sum = a;
-	for (int i = 1; d > i; i++) {
-		sum *= b; sum+=c;
+	int a, b, c, d=1;
+	scanf("%d %d %d", &a, &b, &c);
+	while(int i = 1; n > i; i++) {
+		a = a * m + d;
 	}
-	printf("%.0f", sum);
+	printf("%lld", a);
 	return 0;
 }
-
 // (i%10 == 3 || i % 10 == 6 || i % 10 == 9)  - 일의자리 3,6,9
 // 1 121 12421 1248421 || 팰린드롬수 || 2k-1
 
