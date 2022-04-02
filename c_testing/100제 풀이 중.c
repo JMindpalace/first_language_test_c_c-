e82431e236f95b20c97cc4f94f804a373cc6a31d
Kim Jae Hui <reysin960205@gmail.com>
2022-04-02 15:11:16 +0900

    friday te

--- c_testing/100제 풀이 중.c
+++ c_testing/100제 풀이 중.c
@@ -3,19 +3,20 @@
 
 int main()
 {
-	int a[10], big=0, min=0;
+	int a[10], big=0, min=0, even=0;
 
 	for (int i = 0; i <= 9; i++) {
 		printf("%d번째 수: ", i);
 		scanf("%d", &a[i]);
 		if (a[i] > big) big = a[i];
 		if (a[i] < min) min = a[i];
+		if (a[i]%2==0) even++;
 	}
 	for (int i = 9; i >= 0; i--) {
 		printf("%d번째 수: %d ", i, a[i]);
 		printf("\n ");
 	}
-	printf("가장 큰 수 : %d // 가장 작은 수 : %d ", big, min);
+	printf("가장 큰 수 : %d // 가장 작은 수 : %d // 짝수 수 : %d", big, min, even);
 	return 0;
 }
 
