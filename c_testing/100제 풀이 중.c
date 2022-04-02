9c4b3e45debc0a370cee6a079271f07ae3d20cba
Kim Jae Hui <reysin960205@gmail.com>
2022-04-02 15:06:03 +0900

    friday

--- c_testing/100제 풀이 중.c
+++ c_testing/100제 풀이 중.c
@@ -3,14 +3,19 @@
 
 int main()
 {
-	int a, b, c, d=1;
-	scanf("%d %d %d", &a, &b, &c);
-	while(int i = 1; n > i; i++) {
-		a = a * m + d;
+	int a[10];
+
+	for (int i = 0; i <= 9; i++) {
+		printf("%d번째 수: ", i);
+		scanf("%d", &a[i]);
 	}
-	printf("%lld", a);
+	for (int i = 9; i >= 0; i--) {
+		printf("%d번째 수: %d ", i, a[i]);
+		printf("\n ");
+	}
 	return 0;
 }
+
 // (i%10 == 3 || i % 10 == 6 || i % 10 == 9)  - 일의자리 3,6,9
 // 1 121 12421 1248421 || 팰린드롬수 || 2k-1
 
