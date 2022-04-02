823f8662558c907b4d63a1003e3a70af0cc1658f
Kim Jae Hui <reysin960205@gmail.com>
2022-04-02 18:10:56 +0900

    friday tes

--- c_testing/100제 풀이 중.c
+++ c_testing/100제 풀이 중.c
@@ -3,25 +3,20 @@
 
 int main()
 {
-	int a[10], big=0, min=0, even=0;
+	int n, a[25] = { 0 }, t;
+	scanf("%d", &n);
 
-	for (int i = 0; i <= 9; i++) {
-		printf("%d번째 수: ", i);
-		scanf("%d", &a[i]);
-		if (a[i] > big) big = a[i];
-		if (a[i] < min) min = a[i];
-		if (a[i]%2==0) even++;
+	for (int i = 1; i < n + 1; i++) {
+		scanf("%d ", &t);
+		a[t]++;
 	}
-	for (int i = 9; i >= 0; i--) {
-		printf("%d번째 수: %d ", i, a[i]);
-		printf("\n ");
+	for (int i = 1; i < 24; i++) {
+		printf("%d ", a[i]);
 	}
-	printf("가장 큰 수 : %d // 가장 작은 수 : %d // 짝수 수 : %d", big, min, even);
 	return 0;
 }
-
 // (i%10 == 3 || i % 10 == 6 || i % 10 == 9)  - 일의자리 3,6,9
-// 1 121 12421 1248421 || 팰린드롬수 || 2k-1
+// 1 121 12421 1248421 || 파스칼의 삼각형 || 팰린드롬수 - 2k-1
 
 // git add .
 // git commit -m "history"
