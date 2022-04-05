8d64d3a734b65ad7a97bfc8b8026a0a72937d477
Kim Jae Hui <reysin960205@gmail.com>
2022-04-05 16:55:30 +0900

    1228

--- c_testing/100제 풀이 중.c
+++ c_testing/100제 풀이 중.c
@@ -2,17 +2,20 @@
 #include <stdio.h>
 int main()
 {
-	// 1203 : 비만도 측정 0 -- 비만 판단만하기
-	int a;
-	scanf("%d", &a);
-	if(a <= 10){
-	    printf("정상");
+	// 1228 : 비만도 측정 1 -- 표준몸무게 도입
+	float a, b, c, d;
+	scanf("%f %f", &a, &b);
+	d = (a - 100)*0.9;
+	c = ((b - d) * 100)/d;
+	
+	if (c <= 10.09) {
+		printf("정상");
 	}
-	else if(a <= 20){
-	    printf("과체중");
+	else if (c <= 20) {
+		printf("과체중");
 	}
-	else{
-	    printf("비만");
+	else {
+		printf("비만");
 	}
 	return 0;
 }
