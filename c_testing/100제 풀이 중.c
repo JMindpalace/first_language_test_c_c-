49b39618d42f416a03794fab7183b7049751f68e
Kim Jae Hui <reysin960205@gmail.com>
2022-04-05 20:02:32 +0900

    1163

--- c_testing/100제 풀이 중.c
+++ c_testing/100제 풀이 중.c
@@ -5,7 +5,7 @@ int main()
 	// 1162 : 1의 자리 구하기
 	int a, b, c, result;
 	scanf("%d %d %d", &a, &b, &c);
-	result = (a - b + c)%10;
+	result = (a + b + c)%1000;
 	if (result == 0) { printf("대박"); }
 	else { printf("그럭저럭"); }
 	
