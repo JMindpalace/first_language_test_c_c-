e59bc826c12bd4f283a499f7f2c604655619f976
Kim Jae Hui <reysin960205@gmail.com>
2022-04-05 20:07:10 +0900

    1164

--- c_testing/100제 풀이 중.c
+++ c_testing/100제 풀이 중.c
@@ -2,12 +2,11 @@
 #include <stdio.h>
 int main()
 {
-	// 1162 : 1의 자리 구하기
-	int a, b, c, result;
+	// 1164 : 3개 조건 통과
+	int a, b, c;
 	scanf("%d %d %d", &a, &b, &c);
-	result = (a + b + c)%1000;
-	if (result == 0) { printf("대박"); }
-	else { printf("그럭저럭"); }
+	if (a<=170 || b<=170 || c<=170 ) { printf("CRASH"); }
+	else { printf("PASS"); }
 	
 	return 0;
 }
