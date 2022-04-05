ca6e1cfd4c31ceb65af4197f9c46f86a0ce97f0f
Kim Jae Hui <reysin960205@gmail.com>
2022-04-05 16:53:39 +0900

    1180

--- c_testing/100제 풀이 중.c
+++ c_testing/100제 풀이 중.c
@@ -2,15 +2,17 @@
 #include <stdio.h>
 int main()
 {
-	// 1154 : 큰수 - 작은수
-	int a, b;
-	scanf("%d %d", &a, &b);
-	if(a>=b){ printf("%d", a-b);}
-	else { printf("%d", b-a);}
+	// 1180 : 휴지통 - 입력 수 10과 1자리 바꾸고 *2  && 50이상이하 멘트 출력
+	int a, r;
+	scanf("%d",&a);
+	r = ((a%10 *10) + (a/10))*2;
+
+	if (r >= 100) { r = r - 100; } // r = r%100;
+	printf("%d\n", r);
+	if (r > 50) { printf("OH MY GOD"); }
+	else { printf("GOOD"); }
 	return 0;
 }
-// (i%10 == 3 || i % 10 == 6 || i % 10 == 9)  - 일의자리 3,6,9
-// 1 121 12421 1248421 || 파스칼의 삼각형 || 팰린드롬수 - 2k-1
 
 // git add .
 // git commit -m "
