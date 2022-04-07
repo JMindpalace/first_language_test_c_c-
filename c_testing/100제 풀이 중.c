8198b3098c116b41ed3a309df9b0557ed707d934
Kim Jae Hui <reysin960205@gmail.com>
2022-04-07 22:44:48 +0900

    1204

--- c_testing/100제 풀이 중.c
+++ c_testing/100제 풀이 중.c
@@ -2,13 +2,23 @@
 #include <stdio.h>
 int main()
 {
-	// 1201 정수 판별
-	int d;
-	scanf("%d", &d);
+	// 1201 : trillionth
+	int t;
+	scanf("%d", &t);
 	
-	if(d==0){ printf("0");}
-	else if(d>0){ printf("양수"); }
-	else{ printf("음수"); }
+	if(t%10==1){
+		if(t==11) printf("%dth", t);
+		else printf("%dst", t);
+	}
+	else if(t%10==2){
+		if(t==12) printf("%dth", t);
+		else printf("%dnd", t);
+	}
+	else if(t%10==3){
+		if(t==13) printf("%dth", t);
+		else printf("%drd", t);
+	}
+	else{ printf("%dth", t); }
 
 	return 0;
 }
