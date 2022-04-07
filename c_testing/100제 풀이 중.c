3791c53d509e463e6b2d9deace5c495948423b73
Kim Jae Hui <reysin960205@gmail.com>
2022-04-08 01:02:50 +0900

    1212

--- c_testing/100제 풀이 중.c
+++ c_testing/100제 풀이 중.c
@@ -2,32 +2,35 @@
 #include <stdio.h>
 int main()
 {
-	// 1210 : 칼로리 계산
-	int a, b, sum=0;
-	scanf("%d %d", &a, &b);
+	// 1212 : 삼각형 성립 조건
+	int a, b, c, max;
+	scanf("%d %d %d", &a, &b, &c);
 	
-	switch (a){
-		case 1: {sum+=400; break;}
-		case 2: {sum+=340; break;}
-		case 3: {sum+=170; break;}
-		case 4: {sum+=100; break;}
-		case 5: {sum+=70; break;}
-	}
-
-	switch (b){
-		case 1: {sum+=400; break;}
-		case 2: {sum+=340; break;}
-		case 3: {sum+=170; break;}
-		case 4: {sum+=100; break;}
-		case 5: {sum+=70; break;}
-	}
-
-	if(sum>500) printf("angry");
-	else printf("no angry");
-
+	if(a+b>c && a+c>b && b+c>a) // 3변 모두 등식 만족
+		printf("yes");
+	else
+		printf("no");
+	
 	return 0;
 }
 
 // git add .
 // git commit -m "
 // git push origin master
+
+/*
+	max = c>(a>b ? a : b) ? c : (a<b ? a : b);
+
+	if(max==a) {
+		if(b+c <= a) printf("no");
+		else printf("yes");
+	}
+	else if(max==b) {
+		if(a+c <= b) printf("no");
+		else printf("yes");
+	}
+	else if(max==c){ // max==c
+		if(b+a <= c) printf("no");
+		else printf("yes");
+	}
+*/
