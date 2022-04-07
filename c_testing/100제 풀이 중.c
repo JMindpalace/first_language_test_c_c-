95f24f7233c010049fd4bd94617985435e6d2fdd
Kim Jae Hui <reysin960205@gmail.com>
2022-04-07 20:45:08 +0900

    1173

--- c_testing/100제 풀이 중.c
+++ c_testing/100제 풀이 중.c
@@ -3,24 +3,23 @@
 int main()
 {
 	// 1171 : 학년 반 번호 -- 조건 분기 -- a&b c || a&b&c
-	int grade, class, num;
-	scanf("%d %d %d", &grade, &class, &num);
+	int h, m;
+	scanf("%d %d", &h, &m);
 
-	if (class > 10) {
-		if(num < 10) { printf("%d%d00%d", grade, class, num); }
-		else if (num > 100) { printf("%d%d%d", grade, class, num); }
-		else { printf("%d%d0%d", grade, class, num); }
-	}
-	else {
-		if (num < 10) { printf("%d0%d00%d", grade, class, num); }
-		else if (num > 100) { printf("%d0%d%d", grade, class, num); }
-		else { printf("%d0%d0%d", grade, class, num); }
-	}
+	m = m-30; // t = (t*60 +m) -30;
 	
+	if(m<0){
+		if(h==0) {
+			if(m>-29) { printf("23 %d", 60+m); }
+			else { printf("0 %d", 60+m); }
+		}
+		else printf("%d %d", h-1, 60+m);
+	}
+	else{ printf("%d %d", h, m);}
+
 	return 0;
 }
 
 // git add .
 // git commit -m "
 // git push origin master
-
