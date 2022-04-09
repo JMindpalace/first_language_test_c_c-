16e2de95f45a643d1138a587b2a9ae1ca174ebee
Kim Jae Hui <reysin960205@gmail.com>
2022-04-09 23:47:50 +0900

    1255

--- c_testing/100제 풀이 중.c
+++ c_testing/100제 풀이 중.c
@@ -3,16 +3,16 @@
 int main()
 {
 	// 1253 : a~b 출력 - 차후에 별도 함수 생성
-	int a, b;
-	scanf("%d %d", &a, &b);
+	double a, b;
+	scanf("%lf %lf", &a, &b);
 	
 	if(a>b){
-		for(int i=b; i<=a; i++)
-			printf("%d ", i);
+		for(int i=b; i<=a; i+=0.01)
+			printf("%.2f ", i);
 	}
 	else{
 		for(int i=a; i<=b; i++)
-			printf("%d ", i);
+			printf("%.2f ", i);
 	}
 
 	return 0;
