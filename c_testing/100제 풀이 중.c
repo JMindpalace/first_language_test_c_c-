403a087843a48deec6c05d8e8c4f3578c81d9a75
Kim Jae Hui <reysin960205@gmail.com>
2022-04-09 23:52:51 +0900

    1258

--- c_testing/100제 풀이 중.c
+++ c_testing/100제 풀이 중.c
@@ -2,13 +2,14 @@
 #include <stdio.h>
 int main()
 {
-	// 1257 : 두 수 사이의 홀수 출력 - 차후에 별도 함수 생성
-	int a, b;
-	scanf("%d %d", &a, &b);
+	// 1258 : 1부터 n까지 합 구하기 - 차후에 별도 함수 생성
+	int n, sum=0;
+	scanf("%d", &n);
 
-	if(a%2==0) a++;
-	for (int i=a; i<=b; i+=2)
-			printf("%d", i);
+	for (int i=0; i<=n; i++)
+		sum += i;
+	
+	printf("%d", sum);
 
 	return 0;
 }
