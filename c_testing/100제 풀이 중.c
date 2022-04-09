c131d952cb9327cc3b424183dcf2ab53a6094733
Kim Jae Hui <reysin960205@gmail.com>
2022-04-09 17:53:55 +0900

    배열포인터-1

--- c_testing/100제 풀이 중.c
+++ c_testing/100제 풀이 중.c
@@ -3,31 +3,21 @@
 int main()
 {
 	// 1226 : 로또 프로그램
-	int a[7], m[6], cou = 0, bou = 0;
+	int n, a[100];
+	scanf("%d", &n);
 
-	for (int i = 0; i < 7; i++) { scanf("%d", &a[i]); }
-	for (int i = 0; i < 6; i++) { scanf("%d", &m[i]); }
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &a[i]);
+	}
 
-	for (int i = 0; i < 6; i++) {
-		for (int j = 0; j < 6; j++) {
-			if (a[i] == m[j]) cou++;
-		}
+	for (int i = 1; i < n; i += 2) {
+		printf("%d ", a[i]);
 	}
-	for (int i = 6; i < 7; i++) {
-		for (int j = 0; j < 6; j++) {
-			if (a[i] == m[j]) bou++;
-		}
+	printf("\n");
+	for (int i = 0; i < n; i += 2) {
+		printf("%d ", a[i]);
 	}
-	switch (cou) {
-		case 6: {printf("1"); break; }
-		case 5: {
-			if(bou==1) printf("2");
-			else printf("3");
-			break; }
-		case 4: {printf("4"); break; }
-		case 3: {printf("5"); break; }
-		default: printf("0");
-	}
+
 	return 0;
 }
 
