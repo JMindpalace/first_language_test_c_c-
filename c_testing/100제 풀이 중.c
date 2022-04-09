388a1c66edaa5c8e99643199b44f9bfc096aa04d
Kim Jae Hui <reysin960205@gmail.com>
2022-04-09 23:33:33 +0900

    1231

--- c_testing/100제 풀이 중.c
+++ c_testing/100제 풀이 중.c
@@ -2,15 +2,15 @@
 #include <stdio.h>
 int main()
 {
-	// 1226 : 로또 프로그램
-	int arr[3][4] = { {1,2,3,4} , {5,6,7,8} , {9,10,11,12} }; // arr = &&arr
-	int(*ptr)[4] = arr;
+	//1231 : 간단 계산기
+	int a, b; char c;
+	scanf("%d%c%d", &a, &c, &b);
 
-	for(int i=0; i<3; i++){
-		for(int j=0; j<4; j++){
-			printf("%d", *ptr[i][j]);
-		}
-		printf("\n");
+	switch (c){
+		case '+':{ printf("%d", a+b); break; }
+		case '-':{ printf("%d", a-b); break; }
+		case '*':{ printf("%d", a*b); break; }
+		case '/':{ printf("%.2f", (float)a/b); break; }
 	}
 
 	return 0;
