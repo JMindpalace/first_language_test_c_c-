6320efc1ad3e62968e1223871438f3001ee410a4
Kim Jae Hui <reysin960205@gmail.com>
2022-04-09 19:32:26 +0900

    배열포인터 7

--- c_testing/100제 풀이 중.c
+++ c_testing/100제 풀이 중.c
@@ -3,14 +3,15 @@
 int main()
 {
 	// 1226 : 로또 프로그램
-	int arr[3][3] = { {1,2,3} , {4,5,6} , {7,8,9} }; // arr = &&arr
+	int arr[3][4] = { {1,2,3,4} , {5,6,7,8} , {9,10,11,12} }; // arr = &&arr
+	int(*ptr)[4] = arr;
 
-	printf("%d\n", &arr[0][0]); // 168000 - 1
-
-	printf("%d\n", arr[0]+1); // 168004 - 2
-	printf("%d\n", &arr[0]+1); // 168000
-	printf("%d\n", arr+1); // 168000
-	printf("%d\n", &arr+1); // 168000
+	for(int i=0; i<3; i++){
+		for(int j=0; j<4; j++){
+			printf("%d", *ptr[i][j]);
+		}
+		printf("\n");
+	}
 
 	return 0;
 }
