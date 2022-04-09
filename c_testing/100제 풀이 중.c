e5a55a3eda9e92d4d2a7ff304f37073fd4435738
Kim Jae Hui <reysin960205@gmail.com>
2022-04-10 00:03:08 +0900

    1265

--- c_testing/100제 풀이 중.c
+++ c_testing/100제 풀이 중.c
@@ -3,11 +3,11 @@
 int main()
 {
 	//1261 : 10개의 수 중 5의 배수를 하나만 출력 - 차후에 별도 함수 생성
-	int a[10], check=0;
-	
-	for (int i=0; i<10; i++){
-		scanf("%d", &a[i]);
-		if(a[i]%5 == 0) check ++;
+	int a;
+	scanf("%d", &a);
+
+	for (int i=1; i<10; i++){
+		printf("%d*%d=%d", a,i,a*i);
 	}
 	
 	if(check==0){
@@ -16,7 +16,7 @@ int main()
 	else{
 		for (int i=0; i<10; i++){
 			if(a[i]%5 == 0) {
-				printf("%d", a[i]);
+				
 				break;
 			}
 		}
