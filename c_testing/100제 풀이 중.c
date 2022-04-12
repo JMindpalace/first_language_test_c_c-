f40f9891ab3aaa67616ad8e8332f7a00f30ccc71
Kim Jae Hui <reysin960205@gmail.com>
2022-04-12 13:13:30 +0900

    1272

--- c_testing/100제 풀이 중.c
+++ c_testing/100제 풀이 중.c
@@ -2,14 +2,21 @@
 #include <stdio.h>
 int main()
 {
-	// 1269 : 수열의 값 구하기 - 차후에 별도 함수 생성
-	int n, max, sum = 0;
-	scanf("%d", &n);
+	// 1272 : 기부 - 차후에 별도 함수 생성
+	int k, h, cou = 0, sum = 0;
+	scanf("%d %d", &k, &h);
 
-	for (int i = 0; i <n; i++) {
-		scanf("%d", &max);
-		if(sum<max){ sum = max;}
+	for (int i = 1, cha=0; cou < 2; i++) {
+		if(i%2!=0){ cha++; }
+
+		if (i == k || i == h) {
+			if (i % 2 == 0) { sum = (cha *10)+sum; cou++; }
+			else { sum += cha; cou++; }
+		}
 	}
+
+	// if(k % 2 == 0){	sum += k * 5; } else{ sum += k / 2 + 1; }  -  
+
 	printf("%d", sum);
 	
 	return 0;
