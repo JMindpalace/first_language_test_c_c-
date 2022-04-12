805979a15d7e7562c62ab7df5f9e4cc12a077183
Kim Jae Hui <reysin960205@gmail.com>
2022-04-12 11:56:51 +0900

    1226

--- c_testing/100제 풀이 중.c
+++ c_testing/100제 풀이 중.c
@@ -2,13 +2,13 @@
 #include <stdio.h>
 int main()
 {
-	// 1266 : n개의 수의 합 - 차후에 별도 함수 생성
+	// 1268 : n개의 수 중 짝수의 개수 - 차후에 별도 함수 생성
 	int a, b, sum=0;
 	scanf("%d", &a);
 
 	for (int i=0; i<a; i++){
 		scanf("%d ", &b);
-		if(b%5==0) sum += b;
+		if(b%2==0) sum ++;
 	}
 	printf("%d", sum);
 	
