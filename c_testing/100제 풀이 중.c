5df2a7a5b398607b1d2b01c8ec6b75ebf3270139
Kim Jae Hui <reysin960205@gmail.com>
2022-04-12 13:54:15 +0900

    1277

--- c_testing/100제 풀이 중.c
+++ c_testing/100제 풀이 중.c
@@ -3,13 +3,17 @@
 int main()
 {
 	// 1275 : k 제곱 구하기 - 차후에 별도 함수 생성
-	int n, sum = 1;
+	int n, a, half=0;
 	scanf("%d", &n);
+	if(n%2==0) half = half/2;
+	else half = half/2+1;
 
-	for (int i = 0; i < n; i++) {
-		sum = sum*(n-i);
+	for (int i = 1; i <= n; i++) {
+		scnaf("%d", a);
+		if(i==1 || i==half || i==n)
+			printf("%d", a);
 	}
-	printf("%d", sum);
+	
 
 	// while(n!=0) { sum=sum*n; n--; }
 
