2a1229aef70a69d6694847dbfd91c5a5d2b19392
Kim Jae Hui <reysin960205@gmail.com>
2022-04-12 13:20:27 +0900

    1273

--- c_testing/100제 풀이 중.c
+++ c_testing/100제 풀이 중.c
@@ -3,21 +3,12 @@
 int main()
 {
 	// 1272 : 기부 - 차후에 별도 함수 생성
-	int k, h, cou = 0, sum = 0;
-	scanf("%d %d", &k, &h);
+	int n;
+	scanf("%d", &n);
 
-	for (int i = 1, cha=0; cou < 2; i++) {
-		if(i%2!=0){ cha++; }
-
-		if (i == k || i == h) {
-			if (i % 2 == 0) { sum = (cha *10)+sum; cou++; }
-			else { sum += cha; cou++; }
-		}
+	for (int i = 1; i <= n; i++) {
+		if (n%i==0) printf("%d ", i);
 	}
-
-	// if(k % 2 == 0){	sum += k * 5; } else{ sum += k / 2 + 1; }  -  
-
-	printf("%d", sum);
 	
 	return 0;
 }
