2ad2abc60871bf13bad735867ed81e2e81674357
Kim Jae Hui <reysin960205@gmail.com>
2022-04-12 12:53:15 +0900

    1271

--- c_testing/100제 풀이 중.c
+++ c_testing/100제 풀이 중.c
@@ -3,15 +3,13 @@
 int main()
 {
 	// 1269 : 수열의 값 구하기 - 차후에 별도 함수 생성
-	int a, b, c, d, sum = 0;
-	scanf("%d %d %d %d", &a, &b, &c, &d);
+	int n, max, sum = 0;
+	scanf("%d", &n);
 
-	for (int i = 0; i < d; i++) {
-		if (i == 0) { sum += a; continue;  }
-		sum = a * b + c;
-		a = sum;
+	for (int i = 0; i <n; i++) {
+		scanf("%d", &max);
+		if(sum<max){ sum = max;}
 	}
-	// for(sum += a, i=1; i<d; i++) { sum = s*b+c; }
 	printf("%d", sum);
 	
 	return 0;
