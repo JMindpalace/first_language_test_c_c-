d1725b84db14515a130056bd07d29e739c16e287
Kim Jae Hui <reysin960205@gmail.com>
2022-04-12 13:25:03 +0900

    1274

--- c_testing/100제 풀이 중.c
+++ c_testing/100제 풀이 중.c
@@ -2,14 +2,24 @@
 #include <stdio.h>
 int main()
 {
-	// 1272 : 기부 - 차후에 별도 함수 생성
-	int n;
+	// 1274 : 소수 판별 - 차후에 별도 함수 생성
+	int n, cou=0;
 	scanf("%d", &n);
 
 	for (int i = 1; i <= n; i++) {
-		if (n%i==0) printf("%d ", i);
+		if (n%i==0) cou++;
 	}
 	
+	if(cou==2){ printf("prime"); }
+	else{ printf("not prime"); }
+
+	/*
+	for (int i = 2; i <n; i++) {
+		if (n%i==0) { printf("not prime"); return 0;}
+	}
+	printf("prime");
+	*/
+
 	return 0;
 }
 
