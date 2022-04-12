d594c5453ef4a6c4b977ad4a467a75cb45bd7f6e
Kim Jae Hui <reysin960205@gmail.com>
2022-04-12 14:01:57 +0900

    1280

--- c_testing/100제 풀이 중.c
+++ c_testing/100제 풀이 중.c
@@ -3,14 +3,17 @@
 int main()
 {
 	// 1279 : 홀수는 더하고 짝수는 빼고 1 - 차후에 별도 함수 생성
-	int a, b, sum=0;
+	int a, b, sum=0, max=0, min=0;
 	scanf("%d %d", &a, &b);
 	
-	for (int i=a; i <= b; i++) {
-		if(i%2==0) sum -= i;
-		else sum += i;
+	if(a>b){ max=a; min=b; }
+	else{ max=b; min=a; } 
+
+	for (int i=min; i <= max; i++) {
+		if(i%2==0) {sum -= i; printf("-%d", i); }
+		else { sum += i; printf("+%d", i);}
 	}
-	printf("%d", sum);
+	printf("=%d", sum);
 
 	return 0;
 }
