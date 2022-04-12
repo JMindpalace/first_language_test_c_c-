350a24979358fae68d86575a2f2dc1d87077681c
Kim Jae Hui <reysin960205@gmail.com>
2022-04-12 13:31:39 +0900

    1275

--- c_testing/100제 풀이 중.c
+++ c_testing/100제 풀이 중.c
@@ -2,23 +2,14 @@
 #include <stdio.h>
 int main()
 {
-	// 1274 : 소수 판별 - 차후에 별도 함수 생성
-	int n, cou=0;
-	scanf("%d", &n);
+	// 1275 : k 제곱 구하기 - 차후에 별도 함수 생성
+	int n, k, sum=1;
+	scanf("%d %d", &n, &k);
 
-	for (int i = 1; i <= n; i++) {
-		if (n%i==0) cou++;
+	for (int i = 0; i < k; i++) {
+		sum = sum*n;
 	}
-	
-	if(cou==2){ printf("prime"); }
-	else{ printf("not prime"); }
-
-	/*
-	for (int i = 2; i <n; i++) {
-		if (n%i==0) { printf("not prime"); return 0;}
-	}
-	printf("prime");
-	*/
+	printf("%d", sum);
 
 	return 0;
 }
