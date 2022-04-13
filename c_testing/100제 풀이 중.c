1b2664ea881ae1884f30bca18ef3b97209dfc9d8
Kim Jae Hui <reysin960205@gmail.com>
2022-04-13 23:31:01 +0900

    1287

--- c_testing/100제 풀이 중.c
+++ c_testing/100제 풀이 중.c
@@ -1,19 +1,19 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <math.h>
+
 int main()
 {
-	// 1285 : 계산기 2 - 차후에 별도 함수 생성
-	int a, sum = 0;
+	// 1287 : 구구단을 *로 출력하기 - 차후에 별도 함수 생성
+	int n;
+	scanf("%d", &n);
 
-	scanf("%d", &sum);
-	for (char c = 0; ; ) {
-		scanf("%c", &c); if(c == '='){ printf("%d", sum); return 0; }
-		scanf("%d", &a);
-
-		if (c == '+') { sum += a; printf("+ %d\n", sum); }
-		if (c == '-') { sum -= a; printf("- %d\n", sum); }
-		if (c == '*') { sum *= a; printf("* %d\n", sum); }
-		if (c == '/') { sum /= (float)a; printf("/ %d\n", sum); }
+	for (int i = 1; i < 10; i++) {
+		for (int j = 1; j < (i*n)+1; j++) {
+			printf("*");
+		}
+		printf("\n");
+	}
 
 	return 0;
 }
