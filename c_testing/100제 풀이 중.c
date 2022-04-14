f7a21290f2ff61a5cb202bf9d8a9406657210da9
Kim Jae Hui <reysin960205@gmail.com>
2022-04-14 12:21:32 +0900

    1357

--- c_testing/100제 풀이 중.c
+++ c_testing/100제 풀이 중.c
@@ -4,14 +4,19 @@
 
 int main()
 {
-	//1354 : 삼각형 출력하기 2  - 차후에 별도 함수 생성
-	int n, skip=0;
+	// 1357 : 삼각형 출력하기 4   - 차후에 별도 함수 생성
+	int n;
 	scanf("%d", &n);
 
-	for (int i = n; i > 0; i--, skip--) {
-		for (int j=0, jskip=skip; j < i; j++) {
-			if(jskip<0){ printf("\n"); jskip++; }
-			else { printf("*"); }
+	for (int i = 1; i <= n; i++) {
+		for (int j = 0; j < i; j++) {
+			printf("*");
+		}
+		printf("\n");
+	}
+	for (int i = n-1; i > 0; i--) {
+		for (int j = 0; j < i; j++) {
+			printf("*");
 		}
 		printf("\n");
 	}
