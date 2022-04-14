490260c592b6b5523ec10f00cf56814d4ba7c8e7
Kim Jae Hui <reysin960205@gmail.com>
2022-04-14 10:35:29 +0900

    1356

--- c_testing/100제 풀이 중.c
+++ c_testing/100제 풀이 중.c
@@ -4,14 +4,26 @@
 
 int main()
 {
-	// 1351 : 구구단 출력하기 2 - 차후에 별도 함수 생성
+	// 1356 : 사각형 출력하기 2  - 차후에 별도 함수 생성
 	int n;
 	scanf("%d", &n);
+	// i == 0 || i == n - 1 || j == 0 || j == n - 1
+	for (int i = 0; i < n; i++) {
+		if (i == 0 || i == n - 1) {
+			for (int j = 0; j < n; j++) {
+				printf("*");
+			}
 
-	for (int i=0; i<n; i++) {
-		for (int j = 0; j < n; j++) {
-			printf("*");
 		}
+		else {
+			for (int j = 0; j < n; j++) {
+				if (j == 0 || j == n - 1)
+					printf("*");
+				else
+					printf(" ");
+			}
+		}
+		printf("\n");
 	}
 	return 0;
 }
