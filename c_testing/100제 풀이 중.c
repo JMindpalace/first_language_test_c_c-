57bfa38479c4f7360107cabeecbe34b921b55bb2
Kim Jae Hui <reysin960205@gmail.com>
2022-04-14 11:41:30 +0900

    1353

--- c_testing/100제 풀이 중.c
+++ c_testing/100제 풀이 중.c
@@ -4,29 +4,15 @@
 
 int main()
 {
-	// 1365 : 사각형 출력하기 3  - 차후에 별도 함수 생성
+	// 1353 : 삼각형 출력하기 1  - 차후에 별도 함수 생성
 	int n;
 	scanf("%d", &n);
-	// j==1 || i==1 || i==n || j==n || i==j || i==n-j+1 || n/2+1==i || n/2+1==j
-	for (int i = 0; i < n; i++) { printf("*"); } printf("\n");
-
-	for (int i = 2; i < n; i++) {
-		for (int j = 1; j <= n; j++) {
-			if (i == (n - 2)/2 + 2) {
-				printf("*");
-			}
-			else {
-				if (j == 1 || j == n || j == n - i + 1 || j == i || j == n / 2 + 1)
-				{ printf("*"); }
-				else
-				{ printf(" "); }
-			}
-
-		}
-		printf("\n");
+	
+	for(int i=1; i<=n; i++){
+		for(int j=0; j<i; j++) {
+			printf("*");}
 	}
 
-	for (int i = 0; i < n; i++) { printf("*"); }
 	return 0;
 }
 
