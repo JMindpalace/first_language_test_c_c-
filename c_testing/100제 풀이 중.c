159f6d10742120dacf4fd6384fdfbc42f6408896
Kim Jae Hui <reysin960205@gmail.com>
2022-04-15 14:06:31 +0900

    1371

--- c_testing/100제 풀이 중.c
+++ c_testing/100제 풀이 중.c
@@ -5,25 +5,27 @@
 int main()
 {
 	// 1369 : 빗금 친 사각형 출력하기 - 생각 중
-	int n, k;
-	scanf("%d %d", &n, &k);
-
-	for (int i = 0; i < n; i++) { printf("*"); } printf("\n");
-
-	for (int i = 1; i < n-1; i++) {
-		for (int j=1; j <= n; j++) { // k k*2 k*3 < n
-			if (j == 1 || j==n || j==k-1) {
-				printf("*");
-			}
+	int n;
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j <= n+i; j++) { // n-i == " "
+			if (j == n+i || j == n-i-1) { // (i-1)*2 == " "
+				printf("*"); }
 			else {
-				printf(" ");
-			}
-			k--;
+				printf(" "); }
 		}
-		printf(" %d \n", i);
+		printf("\n");
+	}
+	for (int i = n; i > 0; i--) {
+		for (int j = 0; j < n+i; j++) {
+			if (j == n+i-1 || j == n-i) {
+				printf("*"); }
+			else { 
+				printf(" "); }
+		}
+		printf("\n");
 	}
 
-	for (int i = 0; i < n; i++) { printf("*"); } printf("\n");
 
 	return 0;
 }
