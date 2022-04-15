3f193f3c822a55f907ba8fb8c1fa7c684ca6bc41
Kim Jae Hui <reysin960205@gmail.com>
2022-04-15 12:41:29 +0900

    1361

--- c_testing/100제 풀이 중.c
+++ c_testing/100제 풀이 중.c
@@ -4,20 +4,14 @@
 
 int main()
 {
-	// 1362 : 숫자 피라미드 3   - 차후에 별도 함수 생성
+	// 1361 : 별 계단 만들기 - 차후에 별도 함수 생성
 	int n, cou=0;
 	scanf("%d", &n);
 
-	for (int i = n; i > 0; i--) {
-		for (int j = i; j > 0; j--) {
-			cou++;
-		} } // v = n*(n+1)/2;
-	
-	for (int i = n-1; i >= 0; i--) {
-		for (int j = 0; j < n-i; j++) {
-			printf("%d ", cou--);
-		}
-		printf("\n");
+	for (int i = 0; i < n ; i++) {
+		for (int j = 0; j < i; j++)
+		{ printf(" "); }
+		printf("**\n");
 	}
 
 	return 0;
