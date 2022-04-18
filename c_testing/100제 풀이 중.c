6382f70f9fa02c1234c1456e39d2af85e4fd89cb
Kim Jae Hui <reysin960205@gmail.com>
2022-04-18 11:41:55 +0900

    1403

--- c_testing/100제 풀이 중.c
+++ c_testing/100제 풀이 중.c
@@ -2,26 +2,21 @@
 #include <stdio.h>
 #include <math.h>
 
-int main() // 1405 : 숫자 로테이션 ≒ 값 바꾸기
+int main() // 1403 : 배열 두번 출력하기
 {
-	int n, a[1000], b=0;
+	int n, a[100];
 	scanf("%d", &n);
 
 	for (int i = 0; i < n; i++) {
 		scanf("%d", &a[i]);
 	}
-	// for (j=i , g=1 ; g<=n; j++,g++) { if (j == n) j = 0; printf("%d ", a[j]); }
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			printf("%d ", a[j]);
-			if (j == n -1 ) { b = a[0]; }
-		}
-		printf("\n");
-		for (int j = 0; j < n; j++) {
-			a[j] = a[j+1];
-			if (j == n - 1) { a[j]=b; }
+
+	for(int i=0; i<2; i++){
+		for (int i = 0; i < n; i++) {
+			printf("%d\n", a[i]);
 		}
 	}
+	
 
 	return 0;
 }
