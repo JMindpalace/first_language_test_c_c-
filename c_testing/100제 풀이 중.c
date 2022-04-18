c5b7045ae9bcdf56c6870db65b64c516ad82e450
Kim Jae Hui <reysin960205@gmail.com>
2022-04-18 11:38:57 +0900

    1405

--- c_testing/100제 풀이 중.c
+++ c_testing/100제 풀이 중.c
@@ -2,17 +2,25 @@
 #include <stdio.h>
 #include <math.h>
 
-int main() // 1402 : 거꾸로 출력하기 3
+int main() // 1405 : 숫자 로테이션 ≒ 값 바꾸기
 {
-	int n, a[1000];
+	int n, a[1000], b=0;
 	scanf("%d", &n);
 
-	for(int i=0; i<n; i++){
+	for (int i = 0; i < n; i++) {
 		scanf("%d", &a[i]);
 	}
-
-	for(int i=n-1; i>-1; i--){
-		printf("%d ", a[i]);
+	// for (j=i , g=1 ; g<=n; j++,g++) { if (j == n) j = 0; printf("%d ", a[j]); }
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			printf("%d ", a[j]);
+			if (j == n -1 ) { b = a[0]; }
+		}
+		printf("\n");
+		for (int j = 0; j < n; j++) {
+			a[j] = a[j+1];
+			if (j == n - 1) { a[j]=b; }
+		}
 	}
 
 	return 0;
