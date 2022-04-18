517869c5bf66a9100076b17368eadc775d678a49
Kim Jae Hui <reysin960205@gmail.com>
2022-04-19 08:59:03 +0900

    1218

--- c_testing/100제 풀이 중.c
+++ c_testing/100제 풀이 중.c
@@ -2,22 +2,29 @@
 #include <stdio.h>
 #include <math.h>
 
-int main() // 1205 : 최댓값
+int main() // 1218 : 삼각형 판단하기
 {
-	int a, b, min;
-	scanf("%d %d", &a, &b);
-	int sum = a + b, time = a * b;
-	double powf = pow(a, b), powb = pow(b, a), max = 0;
-	if (a > b) { min = a - b; }
-	else { min = b - a; }
+	int a, b, c;
+	scanf("%d %d %d", &a, &b, &c);
+	double po = a*a + b*b;
 
-	if (sum > max) { max = sum; }
-	if (time > max) { max = time; }	
-	if (min > max) { max = min; }
-	if (powf > max) { max = powf; }
-	if (powb > max) { max = powb; }
-
-	printf("%f", max);
+	if (a+b > c) {
+		if (a == b && b == c) {
+			printf("정삼각형");
+		}
+		else if (a == b || b == c || a == c) {
+			printf("이등변삼각형");
+		}
+		else if (po == c*c) {
+			printf("직각삼각형");
+		}
+		else {
+			printf("삼각형");
+		}
+	}
+	else {
+		printf("삼각형아님");
+	}
 
 	return 0;
 }
