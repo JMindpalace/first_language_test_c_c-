79b4d4509282328958d136b5e0ff0aa034edb87d
Kim Jae Hui <reysin960205@gmail.com>
2022-04-19 09:17:15 +0900

    1270

--- c_testing/100제 풀이 중.c
+++ c_testing/100제 풀이 중.c
@@ -2,32 +2,15 @@
 #include <stdio.h>
 #include <math.h>
 
-int main() // 1214 : 이 달은 며칠까지 있을까?
+int main() // 1270 : 1의 개수는
 {
-	int y, m;
-	scanf("%d %d", &y, &m);
-	// day[]={0,31,28,31,30,31,30,31,31,30,31,30,31};
-	switch (m) {
-	case 1:
-	case 3:
-	case 5:
-	case 7:
-	case 8:
-	case 10:
-	case 12:
-	{ printf("31"); break; }
-	case 4:
-	case 6:
-	case 9:
-	case 11:
-	{ printf("30"); break; }
-	case 2:
-		if (y % 400 == 0 || (y % 4 == 0 && y % 100 != 0)) {
-			printf("29"); break;
-		}
-		else { printf("28"); }
+	int n, cou=0;
+	scanf("%d", &n);
+
+	for (int i = 1; i <= n; i++) {
+		if (i % 10 == 1) { cou++; }
 	}
-	// if ((y%4==0 && y%100!=0) || y%400==0) day[2]++; printf("%d ", day[m]);
+	printf("%d\n", cou);
 
 	return 0;
 }
