a3dbec73debbacf56c821003f866059bd2ba3421
Kim Jae Hui <reysin960205@gmail.com>
2022-04-19 10:03:26 +0900

    1283

--- c_testing/100제 풀이 중.c
+++ c_testing/100제 풀이 중.c
@@ -4,13 +4,30 @@
 
 int main() // 1270 : 1의 개수는
 {
-	int n, cou=0;
-	scanf("%d", &n);
+	float a,sa,b,y;
+	int x;
+    scanf("%f %f",&a,&b);
 
-	for (int i = 1; i <= n; i++) {
-		if (i % 10 == 1) { cou++; }
-	}
-	printf("%d\n", cou);
+    sa=a;
+    for(x=0;x<b;x++)
+    {
+                    scanf("%f",&y);
+                    a*=(100+y)/100;
+    }
+
+    if(a-sa>=0.5||a-sa<=-0.5)
+    {
+                      printf("%.0f\n",a-sa);
+                      if(a-sa>0)
+                               printf("good");
+                      else
+                          printf("bad");
+    }
+    else
+    {
+        printf("0\n"); 
+        printf("same");
+    } 
 
 	return 0;
 }
