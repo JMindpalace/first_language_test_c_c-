49c0b602b515703dd1a6c0b8d14b4720d47fbdb1
Kim Jae Hui <reysin960205@gmail.com>
2022-04-20 17:57:59 +0900

    1464

--- c_testing/100제 풀이 중.c
+++ c_testing/100제 풀이 중.c
@@ -2,17 +2,15 @@
 #include <stdio.h>
 #include <math.h>
 
-int main() // 8 // 1463 : [기초-배열연습] 2차원 배열 순서대로 채우기 1-4
+int main() // 9 // 1464 : [기초-배열연습] 2차원 배열 순서대로 채우기 1-5
 {
-	int n, pr;
-	scanf("%d", &n);
+	int n, m, pr;
+	scanf("%d %d", &n, &m);
+	pr = n * m;
 
-	for (int i = n; i > 0; i--) {
-		printf("%d ", i);
-		pr = i;
-		for (int j = 2; j < n+1; j++) {
-			pr += n;
-			printf("%d ", pr);
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			printf("%d ", pr--);
 		}
 		printf("\n");
 	}
