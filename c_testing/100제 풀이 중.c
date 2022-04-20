702cc861135b61f2531f99e76a1c12819b811aae
Kim Jae Hui <reysin960205@gmail.com>
2022-04-20 17:15:33 +0900

    1442

--- c_testing/100제 풀이 중.c
+++ c_testing/100제 풀이 중.c
@@ -2,31 +2,26 @@
 #include <stdio.h>
 #include <math.h>
 
-int main() // 1 // 1440 : 비교
+int main() // 3 // 1442 : 선택(가장 작은 원소를 찾아 첫번째 위치로 옮기고, 남은 원소를 또 탐색) 정렬
 {
-	int n, temp, a[10001];
+	int n, temp, min, a[10001];
 	scanf("%d", &n);
 
 	for (int i = 1; i <= n; i++) {
-		scanf("%d", &a[i]);
-	}
+		scanf("%d", &a[i]); }
 
 	for (int i = 1; i < n; i++) {
-		for (int j = 1; j < n; j++)
-		{
-			if (a[j] > a[j + 1])
-			{
-				temp = a[j];
-				a[j] = a[j + 1];
-				a[j + 1] = temp;
-			}
+		min = i;
+		for (int j = i+1; j <= n; j++) {
+			if (a[j] < a[min]) { min = j; }
 		}
+		temp = a[i];
+		a[i] = a[min];
+		a[min] = temp;
 	}
 
 	for (int i = 1; i <= n; i++) {
-		printf("%d\n", a[i]);
-	}
-
+		printf("%d\n", a[i]); }
 	return 0;
 }
 
