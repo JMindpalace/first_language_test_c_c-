039ea15940e817ade97346306df152fbf72ea9f9
Kim Jae Hui <reysin960205@gmail.com>
2022-04-20 16:56:15 +0900

    1441

--- c_testing/100제 풀이 중.c
+++ c_testing/100제 풀이 중.c
@@ -2,25 +2,29 @@
 #include <stdio.h>
 #include <math.h>
 
-int main() // 1440 : 비교
+int main() // 1 // 1440 : 비교
 {
-	int n, a[1000];
-	scanf("%d", &n); // int a[n];
+	int n, temp, a[10001];
+	scanf("%d", &n);
 
-	for (int i = 0; i < n; i++) {
+	for (int i = 1; i <= n; i++) {
 		scanf("%d", &a[i]);
 	}
 
-	for (int i = 0; i < n; i++) {
-		printf("%d: ", i+1);
-		for (int j = 0; j < n; j++) {
-			if (i==j) { continue; }
-
-			if (a[i] > a[j]) { printf("> "); }
-			else if (a[i] < a[j]) { printf("< "); }
-			else { printf("= "); }
+	for (int i = 1; i < n; i++) {
+		for (int j = 1; j < n; j++)
+		{
+			if (a[j] > a[j + 1])
+			{
+				temp = a[j];
+				a[j] = a[j + 1];
+				a[j + 1] = temp;
+			}
 		}
-		printf("\n");
+	}
+
+	for (int i = 1; i <= n; i++) {
+		printf("%d\n", a[i]);
 	}
 
 	return 0;
