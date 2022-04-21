36b19601a3b0682b267bdf6c512d9f877d5d12b1
Kim Jae Hui <reysin960205@gmail.com>
2022-04-21 15:49:06 +0900

    1469

--- c_testing/100제 풀이 중.c
+++ c_testing/100제 풀이 중.c
@@ -5,29 +5,28 @@
 #include <stdio.h>
 #include <math.h>
 
-int main() // 8 // 1468 : [기초-배열연습] 2차원 배열 지그재그 채우기 2-1
+int main() // 9 // 1469 : [기초-배열연습] 2차원 배열 지그재그 채우기 2-2
 {
-	int n, pr;
+	int n, stan;
 	scanf("%d", &n);
 
 	for (int i = 1; i <= n; i++) {
 		if (i % 2 != 0) {
-			pr = (n*i)-n+1;
-			for (int j = 1; j <= n; j++) {
-				printf("%d ", pr++);
-			}
-			printf("\n");
-		}
-		else {
-			pr = n * i;
-			for (int j = 0; j < n; j++) {
+			stan = i * n;
+			for (int j = 0, pr=stan; j < n; j++) {
 				printf("%d ", pr--);
 			}
 			printf("\n");
 		}
+		else {
+			for (int j = 0, pr = stan; j < n; j++) {
+				printf("%d ", ++pr);
+			}
+			printf("\n");
+		}
 	}
-
 	return 0;
+}
 
 // git add .
 // git commit -m "
