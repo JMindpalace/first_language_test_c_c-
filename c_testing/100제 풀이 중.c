99ee9100ab3133297a42d49af508e8292ace0a5b
Kim Jae Hui <reysin960205@gmail.com>
2022-04-21 14:36:47 +0900

    bad gateway

--- c_testing/100제 풀이 중.c
+++ c_testing/100제 풀이 중.c
@@ -2,18 +2,18 @@
 #include <stdio.h>
 #include <math.h>
 
-int main() // 10 // 1465 : [기초-배열연습] 2차원 배열 순서대로 채우기 1-6
+int main() // 10 // 502 Bad Gateway -- nginx/1.21.0
 {
 	int n, m, pr;
 	scanf("%d %d", &n, &m);
-	pr = (n * m)-m+1;
 
 	for (int i = 0; i < n; i++) {
+			pr = (n*m)-i;
 		for (int j = 0; j < m; j++) {
-			printf("%d ", pr++);
+			printf("%d ", pr);
+			pr -=n;
 		}
 		printf("\n");
-		pr = (pr-m*2);
 	}
 
 	return 0;
