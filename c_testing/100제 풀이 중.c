494ff00e91f4bd388bd9ac864bb3da23c1becfd3
Kim Jae Hui <reysin960205@gmail.com>
2022-04-22 15:22:41 +0900

    1472

--- c_testing/100제 풀이 중.c
+++ c_testing/100제 풀이 중.c
@@ -5,27 +5,27 @@
 #include <stdio.h>
 #include <math.h>
 
-int main() { // 2 // 1471 : [기초-배열연습] 2차원 배열 지그재그 채우기 2-4
+int main() { // 3 // 1472 : [기초-배열연습] 2차원 배열 지그재그 채우기 2-5
 
-	int n, x = 1, a[100][100] = {0};
-	scanf("%d", &n);
+	int Arr[101][101], n, m, x=1;
+	scanf("%d %d", &n, &m);
 
-	for (int i = 0; i < n; i++) {
-		if (i % 2 == 1) {
-			for (int j = 0; j < n; j++) {
-				a[j][i] = x++;
+	for (int i = n, k=1; i >= 1; i--, k++) {
+		if (k % 2 == 0) {
+			for (int j = 1; j <= m; j++) {
+				Arr[i][j] = x++;
 			}
 		}
 		else {
-			for (int j = n-1; j >= 0; j--) {
-				a[j][i] = x++;
+			for (int j = m; j >= 1; j--) {
+				Arr[i][j] = x++;
 			}
 		}
 	}
 
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			printf("%d ", a[i][j]);
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= m; j++) {
+			printf("%d ", Arr[i][j]);
 		}
 		printf("\n");
 	}
